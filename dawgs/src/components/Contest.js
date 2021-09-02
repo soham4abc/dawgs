665aea2280c2b01105dd9e1f7f11786881e2d0f0
Soham Banerjee <sohambanerjee4abc@hotmail.com>
2021-09-02 08:29:42 +0530

    contest page added

--- dawgs/src/components/Contest.js
+++ dawgs/src/components/Contest.js
@@ -1,8 +1,53 @@
-function Contests(){
-    return(
-        <div className="container">
-        <h1>cont</h1>
-      </div>
-    )
+import { Component } from 'react';
+import PostData from './data.json'
+import './display.css'
+class Contests extends Component{
+    render(){
+        return(
+            <div>
+                <h1>Hello</h1>
+                <table class="table">
+  <thead>
+    <tr>
+      
+      <th scope="col">Competition</th>
+      <th scope="col">Link</th>
+      
+      <th scope="col">Start Time</th>
+      <th scope="col">End Time</th>
+      <th scope="col">Platform</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      
+      <td>{PostData.map((postDetail,index)=>{
+                   return<h6>{postDetail.name}</h6> 
+                })}</td>
+      <td>{PostData.map((postDetail,index)=>{
+               
+                return <div ><a href={postDetail.url} target="_blank">link</a></div>
+                })}</td>
+      <td>{PostData.map((postDetail,index)=>{
+                   return<h6>{postDetail.site}</h6> 
+                })}</td>
+        <td>{PostData.map((postDetail,index)=>{
+                   return<h6>{postDetail.start_time}</h6> 
+                })}</td>
+        <td>{PostData.map((postDetail,index)=>{
+                   return<h6>{postDetail.end_time}</h6> 
+                })}</td>
+    </tr>    
+  </tbody>
+</table>
+
+                {/* {PostData.map((postDetail,index)=>{
+                   return<h2>{postDetail.name}</h2> 
+                })} */}
+            </div>
+        )
+    }
 }
+
+
 export default Contests;

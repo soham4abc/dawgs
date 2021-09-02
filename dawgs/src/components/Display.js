dd9b8b1fa023f55174224182d9e326f08d43139f
ankitmalakar7 <malamalakar786@gmail.com>
2021-09-02 08:59:33 +0530

    home added

--- dawgs/src/components/Display.js
+++ dawgs/src/components/Display.js
@@ -1,11 +1,299 @@
 import React from "react";
 import "./display.css";
-function display(){
-    return(
-        <div className="container">
-        <h1>This is an app from our own mehnat</h1>
+<link
+  href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css"
+  rel="stylesheet"
+  integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We"
+  crossorigin="anonymous"
+></link>;
+function display() {
+  return (
+    <>
+      <div
+        id="carouselExampleCaptions"
+        class="carousel slide"
+        data-bs-ride="carousel"
+      >
+        <div class="carousel-indicators">
+          <button
+            type="button"
+            data-bs-target="#carouselExampleCaptions"
+            data-bs-slide-to="0"
+            class="active"
+            aria-current="true"
+            aria-label="Slide 1"
+          ></button>
+          <button
+            type="button"
+            data-bs-target="#carouselExampleCaptions"
+            data-bs-slide-to="1"
+            aria-label="Slide 2"
+          ></button>
+          <button
+            type="button"
+            data-bs-target="#carouselExampleCaptions"
+            data-bs-slide-to="2"
+            aria-label="Slide 3"
+          ></button>
+        </div>
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <img
+              src="https://source.unsplash.com/1200x400/?books"
+              class="d-block w-100"
+              alt="..."
+            />
+          </div>
+          <div class="carousel-item">
+            <img
+              src="https://source.unsplash.com/1200x400/?tech,code"
+              class="d-block w-100"
+              alt="..."
+            />
+          </div>
+          <div class="carousel-item">
+            <img
+              src="https://source.unsplash.com/1200x400/?phones,laptops"
+              class="d-block w-100"
+              alt="..."
+            />
+          </div>
+        </div>
+        <button
+          class="carousel-control-prev"
+          type="button"
+          data-bs-target="#carouselExampleCaptions"
+          data-bs-slide="prev"
+        >
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Previous</span>
+        </button>
+        <button
+          class="carousel-control-next"
+          type="button"
+          data-bs-target="#carouselExampleCaptions"
+          data-bs-slide="next"
+        >
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Next</span>
+        </button>
       </div>
-    )
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-4">
+            <img
+              class="bd-placeholder-img rounded-circle"
+              width="140"
+              height="140"
+              src="https://source.unsplash.com/140x140/?admin"
+              alt=""
+            />
+
+            <h2>
+              Administration
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="currentColor"
+                class="bi bi-bank2"
+                viewBox="0 0 16 16"
+              >
+                <path d="M8.277.084a.5.5 0 0 0-.554 0l-7.5 5A.5.5 0 0 0 .5 6h1.875v7H1.5a.5.5 0 0 0 0 1h13a.5.5 0 1 0 0-1h-.875V6H15.5a.5.5 0 0 0 .277-.916l-7.5-5zM12.375 6v7h-1.25V6h1.25zm-2.5 0v7h-1.25V6h1.25zm-2.5 0v7h-1.25V6h1.25zm-2.5 0v7h-1.25V6h1.25zM8 4a1 1 0 1 1 0-2 1 1 0 0 1 0 2zM.5 15a.5.5 0 0 0 0 1h15a.5.5 0 1 0 0-1H.5z" />
+              </svg>
+            </h2>
+            <p>
+              For handling the whole Administrative works of the institution
+            </p>
+            <p>
+              <a class="btn btn-primary" href="#">
+                View details »
+              </a>
+            </p>
+          </div>
+          <div class="col-lg-4">
+            <img
+              class="bd-placeholder-img rounded-circle"
+              width="140"
+              height="140"
+              src="https://source.unsplash.com/140x140/?classroom"
+              alt=""
+            />
+
+            <h2>Teacher/Professors</h2>
+            <p>
+              For Teachers/Professors- An alternative way of teaching as well as
+              learning.
+            </p>
+            <p>
+              <a class="btn btn-success" href="#">
+                View details »
+              </a>
+            </p>
+          </div>
+          <div class="col-lg-4">
+            <img
+              class="bd-placeholder-img rounded-circle"
+              width="140"
+              height="140"
+              src="https://source.unsplash.com/140x140/?technology,apple"
+              alt=""
+            />
+
+            <h2>Student's Corner</h2>
+            <p>
+              And lastly this, the third column is one and only for students.
+            </p>
+            <p>
+              <a class="btn btn-danger" href="#">
+                View details »
+              </a>
+            </p>
+          </div>
+        </div>
+        <hr class="featurette-divider" />
+
+        <div class="row featurette">
+          <div class="col-md-7">
+            <h2 class="featurette-heading">
+              First featurette heading.
+              <span class="text-muted">It’ll blow your mind.</span>
+            </h2>
+            <p class="lead">
+              Some great placeholder content for the first featurette here.
+              Imagine some exciting prose here.
+            </p>
+          </div>
+          <div class="col-md-5">
+            <svg
+              class="
+              bd-placeholder-img bd-placeholder-img-lg
+              featurette-image
+              img-fluid
+              mx-auto
+            "
+              width="500"
+              height="300"
+              xmlns="http://www.w3.org/2000/svg"
+              role="img"
+              aria-label="Placeholder: 500x300"
+              preserveAspectRatio="xMidYMid slice"
+              focusable="false"
+            >
+              <title>Placeholder</title>
+              <rect width="100%" height="100%" fill="#eee"></rect>
+              <text x="50%" y="50%" fill="#aaa" dy=".3em">
+                500x300
+              </text>
+            </svg>
+          </div>
+        </div>
+
+        <hr class="featurette-divider" />
+
+        <div class="row featurette">
+          <div class="col-md-7 order-md-2">
+            <h2 class="featurette-heading">
+              Oh yeah, it’s that good.
+              <span class="text-muted">See for yourself.</span>
+            </h2>
+            <p class="lead">
+              Another featurette? Of course. More placeholder content here to
+              give you an idea of how this layout would work with some actual
+              real-world content in place.
+            </p>
+          </div>
+          <div class="col-md-5 order-md-1">
+            <svg
+              class="
+              bd-placeholder-img bd-placeholder-img-lg
+              featurette-image
+              img-fluid
+              mx-auto
+            "
+              width="500"
+              height="300"
+              xmlns="http://www.w3.org/2000/svg"
+              role="img"
+              aria-label="Placeholder: 500x500"
+              preserveAspectRatio="xMidYMid slice"
+              focusable="false"
+            >
+              <title>Placeholder</title>
+              <rect width="100%" height="100%" fill="#eee"></rect>
+              <text x="50%" y="50%" fill="#aaa" dy=".3em">
+                500x300
+              </text>
+            </svg>
+          </div>
+        </div>
+
+        <hr class="featurette-divider" />
+
+        <div class="row featurette">
+          <div class="col-md-7">
+            <h2 class="featurette-heading">
+              And lastly, this one. <span class="text-muted">Checkmate.</span>
+            </h2>
+            <p class="lead">
+              And yes, this is the last block of representative placeholder
+              content. Again, not really intended to be actually read, simply
+              here to give you a better view of what this would look like with
+              some actual content. Your content.
+            </p>
+          </div>
+          <div class="col-md-5">
+            <svg
+              class="
+              bd-placeholder-img bd-placeholder-img-lg
+              featurette-image
+              img-fluid
+              mx-auto
+            "
+              width="500"
+              height="300"
+              xmlns="http://www.w3.org/2000/svg"
+              role="img"
+              aria-label="Placeholder: 500x500"
+              preserveAspectRatio="xMidYMid slice"
+              focusable="false"
+            >
+              <title>Placeholder</title>
+              <rect width="100%" height="100%" fill="#eee"></rect>
+              <text x="50%" y="50%" fill="#aaa" dy=".3em">
+                500x300
+              </text>
+            </svg>
+          </div>
+        </div>
+
+        <hr class="featurette-divider" />
+      </div>
+      <footer class="container">
+        <p class="float-end">
+          <a href="#">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="30"
+              height="30"
+              fill="currentColor"
+              class="bi bi-arrow-bar-up"
+              viewBox="0 0 16 16"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M8 10a.5.5 0 0 0 .5-.5V3.707l2.146 2.147a.5.5 0 0 0 .708-.708l-3-3a.5.5 0 0 0-.708 0l-3 3a.5.5 0 1 0 .708.708L7.5 3.707V9.5a.5.5 0 0 0 .5.5zm-7 2.5a.5.5 0 0 1 .5-.5h13a.5.5 0 0 1 0 1h-13a.5.5 0 0 1-.5-.5z"
+              />
+            </svg>
+          </a>
+        </p>
+        <p>
+          © 2020–2021 Company, Inc. · <a href="#">Privacy</a> ·{" "}
+          <a href="#">Terms</a>
+        </p>
+      </footer>
+    </>
+  );
 }
 
 export default display;

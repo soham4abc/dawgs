7d4180744f77a8b90b9cbe31452f3bcf3237730c
smart-worker <sarkarsoham07@gmail.com>
2021-09-03 09:14:47 +0530

    build done

--- dawgs/src/App.js
+++ dawgs/src/App.js
@@ -1,10 +1,31 @@
-import './App.css';
-import './components/Home';
-import Home from './components/Home';
+import "./App.css";
+import Home from "./components/Home";
 
 function App() {
   return (
-    <Home></Home>
+    <>
+      <Home></Home>
+      <footer className="container">
+        <p className="float-end">
+          <a href="#">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="30"
+              height="30"
+              fill="currentColor"
+              className="bi bi-arrow-bar-up"
+              viewBox="0 0 16 16"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M8 10a.5.5 0 0 0 .5-.5V3.707l2.146 2.147a.5.5 0 0 0 .708-.708l-3-3a.5.5 0 0 0-.708 0l-3 3a.5.5 0 1 0 .708.708L7.5 3.707V9.5a.5.5 0 0 0 .5.5zm-7 2.5a.5.5 0 0 1 .5-.5h13a.5.5 0 0 1 0 1h-13a.5.5 0 0 1-.5-.5z"
+              />
+            </svg>
+          </a>
+        </p>
+        <p>© 2021 made with love by DaWGs</p>
+      </footer>
+    </>
   );
 }
 
